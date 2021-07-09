e6712796934695417eb5262a3d66097e366f94e2
A-GHOSH-dev <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-07-10 01:22:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -39,3 +39,36 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// demo or it did not happen
+
+var demo1 = document.querySelector('#demo-1')
+var demo2 = document.querySelector('#demo-2')
+
+setTimeout(() => { demo1.checked = true }, 500)
+setTimeout(() => { demo1.checked = false}, 1500)
+setTimeout(() => { demo2.checked = true }, 2000)
+setTimeout(() => { demo2.checked = false}, 3000)
